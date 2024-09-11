d144696622aad834c66beadece96917cae681341
Antoine SEIN <142824551+asein-sinch@users.noreply.github.com>
2024-09-11 19:25:35 +0200

    DEVEXP-555: Initialize snippets repository

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sinch-sdk-node-snippets
+
+Sinch Node.js SDK Code Snippets Repository
+
+This repository contains code snippets related to the [Sinch Node.js SDK](https://github.com/sinch/sinch-sdk-node) usage.
---
+++ eslint.config.mjs
@@ -0,0 +1,62 @@
+import deprecation from 'eslint-plugin-deprecation';
+import prettier from 'eslint-plugin-prettier';
+import node from 'eslint-plugin-node';
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [...compat.extends('eslint:recommended', 'google', 'prettier'), {
+  plugins: {
+    deprecation,
+    prettier,
+    node,
+  },
+
+  languageOptions: {
+    globals: {
+      ...globals.node,
+    },
+
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    semi: 'warn',
+    'comma-dangle': 'warn',
+    quotes: ['warn', 'single'],
+    curly: ['error', 'all'],
+    indent: ['error', 2],
+
+    'object-curly-spacing': ['error', 'always', {
+      objectsInObjects: true,
+      arraysInObjects: true,
+    }],
+
+    'require-jsdoc': ['off'],
+    'operator-linebreak': ['error', 'before'],
+
+    'max-len': ['error', {
+      code: 120,
+      ignoreUrls: true,
+      ignoreComments: true,
+      ignoreTemplateLiterals: true,
+      ignoreRegExpLiterals: true,
+      ignorePattern: '^import.+|test',
+    }],
+
+    'new-cap': 'off',
+  },
+}];
